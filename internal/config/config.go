1f8597262df1f12fb5657a13308d4468d587c534
Roman <roma3io@mail.ru>
2025-05-07 14:12:31 +0700

    basic config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"time"
+)
+
+type Config struct {
+	Env        string     `yaml:"env" env-default:"local"`
+	GRPCServer GRPCServer `yaml:"grpc_server"`
+}
+
+type GRPCServer struct {
+	Port    int           `yaml:"port" env-default:"50051"`
+	Timeout time.Duration `yaml:"timeout" env-default:"5s"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var cfg Config
+	err := cleanenv.ReadConfig(path, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	err = cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
