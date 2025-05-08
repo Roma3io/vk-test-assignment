4f7206ee67794dc82b05caf213f8c7602edc435f
Roman <roma3io@mail.ru>
2025-05-08 12:18:33 +0700

    start server implemented

--- internal/pubsubservice/server.go
+++ internal/pubsubservice/server.go
@@ -1,15 +1,19 @@
 package pubsubservice
 
 import (
+	"context"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"net"
+	"strconv"
 	"vk-test-assignment/internal/config"
-	proto "vk-test-assignment/internal/proto/gen"
+	pb "vk-test-assignment/internal/proto/gen"
 	"vk-test-assignment/pkg/subpub"
 )
 
 type PubSubServer struct {
-	proto.UnimplementedPubSubServer
+	pb.UnimplementedPubSubServer
 	bus        subpub.SubPub
 	config     *config.Config
 	grpcServer *grpc.Server
@@ -24,8 +28,27 @@ func NewPubSubServer(bus subpub.SubPub, config *config.Config, log *zap.Logger) 
 	}
 }
 
-func (s *PubSubServer) Start() error { return nil }
+func (s *PubSubServer) Start() error {
+	listener, err := net.Listen("tcp", strconv.Itoa(s.config.GRPCServer.Port))
+	if err != nil {
+		s.log.Fatal("Error making listener", zap.Error(err))
+		return err
+	}
+	s.grpcServer = grpc.NewServer()
+	pb.RegisterPubSubServer(s.grpcServer, s)
+	s.log.Info("starting gRPC server", zap.Int("port", s.config.GRPCServer.Port))
+	go func() {
+		if err := s.grpcServer.Serve(listener); err != nil {
+			s.log.Error("gRPC server error", zap.Error(err))
+		}
+	}()
+	return nil
+}
 
-func (s *PubSubServer) Subscribe() {}
+func (s *PubSubServer) Subscribe(req *pb.SubscribeRequest, stream pb.PubSub_SubscribeServer) error {
+	return nil
+}
 
-func (s *PubSubServer) Publish() {}
+func (s *PubSubServer) Publish(ctx context.Context, req *pb.PublishRequest) (*emptypb.Empty, error) {
+	return nil, nil
+}
